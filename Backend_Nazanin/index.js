bbea8d0391bc1de9a6fadd3f9ff9f46aab44472a
nazanin <nazi.naghibi67@yahoo.com>
2021-04-08 14:56:02 -0400

    server side

---
+++ Backend_Nazanin/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const userRoute = require('./routes/api/userRoute');
+const authRoute = require('./routes/api/authRoute');
+const flightRoute = require('./routes/api/flightRoute');
+const raserRoute = require('./routes/api/raserRoute');
+const postRoutes = require('./routes/api/posts');
+const connectDB = require('./config/connectDB');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/api/users');
+
+const app = express();
+
+//coneect to db
+connectDB();
+
+//set a middleware to parse dat
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/flight',flightRoute);
+app.use('/api/reserve',raserRoute);
+app.use('/posts',postRoutes);
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Server started');
+});
+
